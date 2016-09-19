e86b584c3a9bd893a989f1501a0efd97955e531b
Mark Feaver <mark.feaver@gmail.com>
2016-09-19 18:51:32 +1200

    Made doors open and close

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    public GameObject innerDoor;
+    
+    int playersInRange = 0;
+    
+    void OnTriggerEnter (Collider other) {
+        if (other.tag != "Player") {
+            return;
+        }
+
+        playersInRange++;
+        if (playersInRange > 0) {
+            OpenDoor ();
+        }
+    }
+
+    void OnTriggerExit (Collider other) {
+        if (other.tag != "Player") {
+            return;
+        }
+
+        playersInRange--;
+        if (playersInRange <= 0) {
+            playersInRange = 0;
+            CloseDoor ();
+        }
+    }
+
+    void OpenDoor () {
+        if (innerDoor.activeSelf) {
+            innerDoor.SetActive (false);
+        }
+    }
+
+    void CloseDoor () {
+        innerDoor.SetActive (true);
+    }
+}
