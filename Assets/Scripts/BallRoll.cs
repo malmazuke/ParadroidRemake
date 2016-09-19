17c19b698758431ef617ff79c98197b974cc8546
Mark Feaver <mark.feaver@gmail.com>
2016-09-19 17:29:57 +1200

    Added a moveable "character"

---
+++ Assets/Scripts/BallRoll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallRoll : MonoBehaviour {
+
+    public float movementForce = 10.0f;
+
+    Rigidbody rb;
+
+    void Start () {
+        rb = GetComponent<Rigidbody> ();
+    }
+
+    void Update () {
+        var xAxis = Input.GetAxis ("Horizontal") * movementForce;
+        var zAxis = Input.GetAxis ("Vertical") * movementForce;
+
+        rb.AddForce (new Vector3 (xAxis, 0.0f, zAxis));
+    }
+}
